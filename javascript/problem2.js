efc33de4123688bc5a3499f59ab50fa706f60953
Ben Hepworth <b.hepworth@cablelabs.com>
2024-07-16 15:55:48 -0600

    Add problem 2 in javascript

---
+++ javascript/problem2.js
@@ -0,0 +1,18 @@
+console.log("#######################");
+console.log("Project Euler Problem 2");
+console.log("#######################\n");
+
+let num1 = 0;
+let num2 = 1;
+let latestNum = 0;
+let sumEvens = 0;
+
+while (latestNum < 4000000) {
+  if (latestNum % 2 == 0) {
+    sumEvens = sumEvens + latestNum;
+  }
+  num1 = num2;
+  num2 = latestNum;
+  latestNum = num1 + num2;
+}
+console.log("Fibonacci sequence even sum - " + sumEvens);
